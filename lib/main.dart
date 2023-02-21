36d496b9b028a40ed05eb69ea28b7b9b9d2a3dbc
MochCahyahadiF05 <cahyadi_254@smkassalaambandung.sch.id>
2023-02-21 15:35:49 +0700

    sa

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:latihan_flutter/column_widget.dart';
+import 'package:latihan_flutter/container_widget.dart';
+import 'package:latihan_flutter/home.dart';
+import 'package:latihan_flutter/row_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Latihan Flutter",
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Home'),
+          backgroundColor: Colors.cyan,
+        ),
+        body: Home(),
+        // body: Column(
+        //   children: [
+        //     RowWidget(),
+        //     Padding(
+        //       padding: EdgeInsets.only(top: 10),
+        //     ),
+        //     ColumnWidget(),
+        //     Padding(
+        //       padding: EdgeInsets.only(top: 10),
+        //     ),
+        //     ContainerWidget(),
+        //   ],
+        // )),
+      ),
+    );
+  }
+}
+
+class HelloWidget extends StatelessWidget {
+  const HelloWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Hello World',
+        style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.blue,
+            backgroundColor: Colors.black12),
+      ),
+    );
+  }
+}
